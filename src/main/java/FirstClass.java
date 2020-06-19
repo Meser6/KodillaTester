c7e69c93cf2bf60a67f2184eb8167b038572def2
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2020-06-19 09:19:37 +0200

    2,4 obiekty grades

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,18 +1,21 @@
 public class FirstClass {
     public static void main(String[] args) {
-        Notebook notebook = new Notebook("600g", 1000, 2015);
+        Notebook notebook = new Notebook(600, 1000, 2015);
 
         System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year);
-        Notebook heavyNotebook = new Notebook("2000g", 1500, 2018);
+        Notebook heavyNotebook = new Notebook(2000, 1500, 2018);
         notebook.checkPrice();
         notebook.prof();
+        notebook.checkWeigth();
         System.out.println(heavyNotebook.weight+ " " + heavyNotebook.price + " " + notebook.year);
         heavyNotebook.checkPrice();
         heavyNotebook.prof();
-        Notebook oldNotebook = new Notebook("1600g", 500, 2010);
+        heavyNotebook.checkWeigth();
+        Notebook oldNotebook = new Notebook(1600, 500, 2010);
         System.out.println(oldNotebook.weight + " " + oldNotebook.price + " " + notebook.year);
         oldNotebook.checkPrice();
         oldNotebook.prof();
+        oldNotebook.checkWeigth();
 
 
     }
--- src/main/java/Notebook.java
+++ src/main/java/Notebook.java
@@ -1,8 +1,8 @@
 public class Notebook {
-        String weight;
+        int weight;
         int price;
         int year;
-        public Notebook (String weight, int price, int year){
+        public Notebook (int weight, int price, int year){
             this.weight = weight;
             this.price = price;
             this.year = year;
@@ -24,6 +24,13 @@ public class Notebook {
             }
         }
         public void checkWeigth (){
-            // TODO
+            if (this.weight <700){
+                System.out.println("This notebook is very light.");
+            } else if (this.weight >= 700 && this.weight <= 1700){
+                System.out.println("This notebook is heavy");
+            }else {
+                System.out.println("This notebook is very heavy.");
+
+            }
         }
 }
