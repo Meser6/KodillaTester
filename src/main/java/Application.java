cc2d17fd3c748e6e82bfa94afaef3dfe9c98df88
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2020-06-21 13:50:20 +0200

    2,6 refaktoryzacja Application

---
+++ src/main/java/Application.java
@@ -0,0 +1,24 @@
+public class Application {
+
+    String name;
+    double age;
+    double height;
+
+    public Application(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+        Application user1 = new Application("Adam", 40.5, 178);
+
+        if (user1.name != null) {
+            if (user1.age > 30 && user1.height > 160) {
+                System.out.println("User is older than 30 and higher then 160cm");
+            } else {
+                System.out.println("User is younger than 30 or lower than 160cm");
+            }
+        }
+    }
+}
