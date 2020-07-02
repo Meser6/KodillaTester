3c5273b5518774197ddb7b887cd51a26c7355518
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2020-07-02 16:34:31 +0200

    3.6 test kalkulator

--- src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,10 +1,8 @@
 package com.kodilla.basic_assertion;
 
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 public class CalculatorTestSuite {
     @Test
@@ -14,10 +12,10 @@ public class CalculatorTestSuite {
         int b = 8;
         int sumResult = calculator.sum(a, b);
         assertEquals(13, sumResult);
-        int subResult = calculator.sub(a,b);
+        int subResult = calculator.sub(a, b);
         assertEquals(-3, subResult);
         int powResult = calculator.pow(a);
-        assertEquals(25,powResult);
+        assertEquals(25, powResult);
 
     }
 }
