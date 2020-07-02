7d7af68f41cf9952dcae7483f868a9773d6716b2
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2020-07-02 16:27:55 +0200

    3.6 test jednostkowy

---
+++ src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.basic_assertion;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CalculatorTestSuite {
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult);
+        int subResult = calculator.sub(a,b);
+        assertEquals(-3, subResult);
+        int powResult = calculator.pow(a);
+        assertEquals(25,powResult);
+
+    }
+}
