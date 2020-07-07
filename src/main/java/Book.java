e2695219e907687bab384a4db0c525fb6fc670c9
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2020-07-07 20:39:17 +0200

    2.4. tablice grades

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -2,14 +2,20 @@ public class Book {
     private String author;
     private String title;
 
-    public static String of(String author, String title) {
-        return "Author: " + author + ", title: " + title;/*TODO Review kdrzazga - Excercise says "Metoda ta ma zwracać
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+    public Book of(String author, String title){
+
+        retu;
+
+        System.out.println("Author: " + author + ", title: " + title);/*TODO Review kdrzazga - Excercise says "Metoda ta ma zwracać
          utworzony obiekt książki" - should return Book, not String*/
     }
 
 
     public static void main(String[] args) {
-        Book book = new Book();
-        String book1 = Book.of("Rowling", "Harry Potter");
+        Book book = new Book("Rowling", "Harry Potter");
     }
 }
