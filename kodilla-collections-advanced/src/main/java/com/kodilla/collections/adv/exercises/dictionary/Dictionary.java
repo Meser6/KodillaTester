6fcae159d79733141f7e83f79b912b2dc3019d0f
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2020-07-27 13:56:06 +0200

    bootcamp

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/dictionary/Dictionary.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/dictionary/Dictionary.java
@@ -6,22 +6,26 @@ public class Dictionary {
     Map<String, List<EnglishWord>> dictionary = new HashMap<>();
 
     public void addWord(String polishWord, EnglishWord englishWord) {
-    List<EnglishWord> englishWords = dictionary.getOrDefault(polishWord, new ArrayList<>());
-    englishWords.add(englishWord);
-    dictionary.put(polishWord, englishWords);
+        List<EnglishWord> englishWords = dictionary.getOrDefault(polishWord, new ArrayList<>());
+        englishWords.add(englishWord);
+        dictionary.put(polishWord, englishWords);
     }
 
     public List<EnglishWord> findEnglishWords(String polishWord) {
-        return Collections.emptyList();
+        return dictionary.getOrDefault(polishWord, Collections.emptyList());
     }
 
     public List<EnglishWord> findEnglishWords(String polishWord, PartOfSpeech partOfSpeech) {
-        return Collections.emptyList();
+        List<EnglishWord> result = new ArrayList<>();
+        for (EnglishWord englishWord : dictionary.getOrDefault(polishWord, Collections.emptyList())) {
+            if (englishWord.getPartOfSpeech().equals(partOfSpeech))
+                result.add(englishWord);
+        }
+        return result;
     }
-    public int size(){
+
+    public int size() {
         return dictionary.size();
     }
 }
-/*
-Metoda ta pobiera z mapy listę angielskich słów, odpowiadających wskazanemu słowu polskiemu. Jeżeli żadnego słowa angielskiego w mapie nie ma, to tworzona jest pusta lista (wykorzystujemy tu metodę getOrDefault z interfejsu Map, która zwraca podaną jako argument wartość domyślną, gdy mapa nie zawiera szukanego przez nas wpisu). Do tej listy (dotychczas istniejących w mapie słów angielskich lub nowej pustej listy) dodawany jest nasz wpis. Następnie wpis ten jest wstawiany do mapy. Metoda put mapy wstawi nowy wpis lub zaktualizuje istniejący, jeżeli wpis o danym kluczu (czyli polskim słowie) był już wcześniej w mapie obecny.
- */
+
