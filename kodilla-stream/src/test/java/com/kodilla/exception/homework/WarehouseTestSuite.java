68c4fdc6574624590f62eee0a367f1a9562b78c9
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2020-07-28 18:06:12 +0200

    6.5

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.exception.homework;
+
+import org.junit.Test;
+
+class WarehouseTestSuite {
+
+    @Test(expected = OrderDoesntExistException.class)
+    public void testWrongOrderNumber() throws Exception {
+        Warehouse warehouse = new Warehouse();
+        warehouse.getOrder("1");
+    }
+
+
+
+
+}
