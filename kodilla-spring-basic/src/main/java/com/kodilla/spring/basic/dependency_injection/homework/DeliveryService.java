70641079b86263e5db5c98e633d09b991fce4a31
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2020-08-21 11:40:42 +0200

    9.1

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/DeliveryService.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/DeliveryService.java
@@ -3,12 +3,5 @@ package com.kodilla.spring.basic.dependency_injection.homework;
 public interface DeliveryService {
 
     public boolean deliverPackage(String address, double weight);// {
-      /*  if (weight > 30) {
-            System.out.println("Package too heavy");
-            return false;
-        }
-        System.out.println("Delivering in progress...");
-        return true;
-    }
-       */
+
 }
