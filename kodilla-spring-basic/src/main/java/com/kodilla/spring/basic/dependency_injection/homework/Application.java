ffeb83d435847dc45e4df8954aaebdd394d52e06
KDRZAZGA <krzysztof.drzazga@op.pl>
2020-08-22 19:17:14 +0200

    9.1 Corrected

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
@@ -1,9 +1,15 @@
 package com.kodilla.spring.basic.dependency_injection.homework;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        ShippingCenter shippingCenter = new ShippingCenter();
-        shippingCenter.sendPackage("Hill Street 11, New York", 18.2);
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.dependency_injection");
+        ShippingCenter shippingCenter = context.getBean(ShippingCenter.class);
+        System.out.println(shippingCenter.sendPackage("Hill Street 11, New York", 18.2));
     }
 }
