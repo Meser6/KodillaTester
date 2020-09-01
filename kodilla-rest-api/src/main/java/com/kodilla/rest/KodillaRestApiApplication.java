4bc170deac2882b0c72cb6e4e6552d7d5a466ac1
Jakub Kaczmarczyk <j.kaczmarczyk95@gmail.com>
2020-09-01 19:00:14 +0200

    10.3

--- kodilla-rest-api/src/main/java/com/kodilla/rest/KodillaRestApiApplication.java
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/KodillaRestApiApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KodillaRestApiApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(KodillaRestApiApplication.class, args);
     }
 }
--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.dependency_injection");
         ShippingCenter shippingCenter = context.getBean(ShippingCenter.class);
         System.out.println(shippingCenter.sendPackage("Hill Street 11, New York", 18.2));
     }
+
+    */
 }
